06eb6ec98b03fc109a4978029a4bea9d95e4b454
yanottamao <oktatama4@gmail.com>
2021-04-30 15:16:01 +0700

    Add random number placement

---
+++ app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gridDisplay = document.querySelector(".grid");
+  const scoreDisplay = document.getElementById("score");
+  const resultDisplay = document.getElementById("result");
+  const width = 4;
+  let squares = [];
+
+  //   create a playing board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      square = document.createElement("div");
+      square.innerHTML = 0;
+      gridDisplay.appendChild(square);
+      squares.push(square);
+    }
+    generate();
+  }
+  createBoard();
+
+  //   generate a number randomly
+  function generate() {
+    let randomNumber = Math.floor(Math.random() * squares.length);
+    if (squares[randomNumber].innerHTML == 0) {
+      squares[randomNumber].innerHTML = 2;
+    } else generate();
+  }
+});
